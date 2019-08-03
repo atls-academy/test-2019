add5c6eb25f5b84f7aa699227f36acc3801639ab
Shlommo <sl@aunited.pro>
2019-08-04 00:17:33 +0600

    feat(theme): update theme styles
    affects: @ui/theme
    
    what's new:
    - borderRadius
    - update colors
    - fontFamily
    - update fontSizes
    - fontWeights
    -
    lineHeights

--- packages/ui/theme/src/theme/fontSizes.ts
+++ packages/ui/theme/src/theme/fontSizes.ts
@@ -1,8 +1,13 @@
 import { ThemeFontSizes } from './types'
 
-const fontSizes: ThemeFontSizes = [14, 16]
+const fontSizes: ThemeFontSizes = [14, 16, 20, 24, 32, 40, 48]
 
 fontSizes.s = fontSizes[0]
 fontSizes.n = fontSizes[1]
+fontSizes.m = fontSizes[2]
+fontSizes.l = fontSizes[3]
+fontSizes.xl = fontSizes[4]
+fontSizes['2xl'] = fontSizes[5]
+fontSizes['3xl'] = fontSizes[6]
 
 export default fontSizes
--- packages/ui/theme/src/theme/index.ts
+++ packages/ui/theme/src/theme/index.ts
@@ -1,9 +1,17 @@
+import borderRadius from './borderRadius'
+import fontFamily from './fontFamily'
 import fontSizes from './fontSizes'
+import fontWeights from './fontWeights'
 import colors from './colors'
+import lineHeights from './lineHeights'
 
 export const theme = {
+  borderRadius,
+  fontFamily,
   fontSizes,
+  fontWeights,
   colors,
+  lineHeights,
 }
 
 export * from './types'
