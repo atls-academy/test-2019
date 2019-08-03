d3a6be0fdd5173416514e30985e1a0519c48d751
Shlommo <sl@aunited.pro>
2019-08-04 00:40:19 +0600

    feat(common): init common and add security reducer
    affects: @aunited/common

---
+++ packages/common/src/reducers/security.ts
@@ -0,0 +1,27 @@
+import storage from 'redux-persist/lib/storage'
+import { persistReducer } from 'redux-persist'
+import { createReducer } from '@utils/reducer'
+import * as actions from '../constants/security'
+
+const initialState = {
+  token: '',
+  expiresIn: '',
+}
+
+const reducer = createReducer(initialState, {
+  [actions.auth]: (state, { token, expiresIn }) => ({
+    token,
+    expiresIn,
+  }),
+  [actions.logout]: () => initialState,
+})
+
+export default persistReducer(
+  {
+    storage,
+    key: 'token',
+    keyPrefix: 'aunited',
+    version: 1,
+  },
+  reducer
+)
